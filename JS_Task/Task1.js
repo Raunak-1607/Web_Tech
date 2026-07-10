71481603d9ef49c9b94655514f9f4269d33c30f6
Raunak-1607 <raunakbiswas537@gmail.com>
2026-07-11 01:04:17 +0600

    JS Task-1 Completed and did some other file modified

---
+++ JS_Task/Task1.js
@@ -0,0 +1,50 @@
+const form = document.getElementById("studentForm");
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const fullname = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const Cpassword = document.getElementById("Cpassword").value;
+    const Pnumber = document.getElementById("Pnumber").value;
+
+
+    document.getElementById("nameError").innerText= "";
+    document.getElementById("emailError").innerText= "";
+    document.getElementById("passwordError").innerText= "";
+    document.getElementById("CpasswordError").innerText= "";
+    document.getElementById("numberError").innerText= "";
+
+    let valid = true;
+
+    if(fullname==""){
+        document.getElementById("nameError").innerText="Invalid Name";
+        valid=false;
+    }
+
+     if(!email.includes("@") || !email.includes(".")){
+        document.getElementById("emailError").innerText="Invalid Email";
+        valid=false;
+    }
+
+
+    if(password.length<6){
+        document.getElementById("passwordError").innerText="Password must be at least 6 characters";
+        valid = false;
+    }
+
+    if(password!==Cpassword){
+        document.getElementById("CpasswordError").innerText="Do not match password";
+        valid = false;
+    }
+
+    if(isNaN(Pnumber) || Pnumber.length<11){
+        document.getElementById("numberError").innerText="Enter valid number";
+        valid = false;
+    }
+
+    if(valid){
+        document.getElementById("success").innerText="Registration Successful!";
+    }
+
+})
